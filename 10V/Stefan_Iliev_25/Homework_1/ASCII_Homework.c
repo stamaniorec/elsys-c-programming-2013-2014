a1e396c3cb78c3398ae36c55c1ed961337108a67
stefcho <junkmen@ubuntu.(none)>
2013-09-26 22:37:09 +0300

    Homework_1 has been completed by Stefan Iliev.

---
+++ 10V/Stefan_Iliev_25/Homework_1/ASCII_Homework.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+
+char Symbol=0;
+int  SymbolNumb=0;
+
+while (SymbolNumb<=255) {
+
+printf("%c - %d\n",Symbol,SymbolNumb);
+Symbol++;
+SymbolNumb++;
+
+
+}
+
+
+return 0;
+}
