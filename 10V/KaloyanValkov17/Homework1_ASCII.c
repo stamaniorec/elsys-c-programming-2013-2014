eed9af6af0b51d2be00b2a95ef7c4ed071b4d819
kdvalkov <kdvalkov@gmail.com>
2013-09-29 13:03:57 +0300

    ASCII by Kaloyan Valkov 10V klas

---
+++ 10V/KaloyanValkov17/Homework1_ASCII.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+int i;
+
+printf("\n\n");
+i = 1;
+while(i<=255) 
+{
+
+printf("\n %d = %c",i,i);
+i++;
+}
+printf("\n\nNapraveno ot Kaloyan Valkov 10V klas (mislq che sum 17 nomer haha)\n\n");
+
+}
+
+/* i = promenlivata mi, izpolzval sum cikul s postuslovie, %d i %c e opredelqneto na promenlivite kato kakuw tip sa i drugo za obqsnenie ne se seshtam */
