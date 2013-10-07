f333ecc892e797d8e16076b8ae7751f9c97d8acb
Georgi Rosenov <hristiyandodov@ubuntu.(none)>
2013-10-07 22:05:11 +0300

    homework 2

---
+++ 10V/Georgi_Rosenov_10/Homework_2/lukas.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main()
+{
+      unsigned long N1=2;
+      unsigned long N2=1;
+      unsigned long N_sum=0;
+      unsigned long N_number=0;
+         
+printf("%lu\n",N1);
+printf("%lu\n",N2);
+
+for(N_number;N_number<100;N_number++)
+        {
+N_sum=N1+N2;
+printf("%lu\n",N_sum);
+N1=N2;
+N2=N_sum;
+}
+
+
+
+return 0;
+}
