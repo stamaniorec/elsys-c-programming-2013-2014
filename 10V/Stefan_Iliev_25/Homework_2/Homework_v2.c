bebe0a07d27f9e3950a0153e380e5a79a0ed2467
Junkmen <mostang1246@gmail.com>
2013-10-07 21:27:24 +0300

    Update Homework_v2.c

--- 10V/Stefan_Iliev_25/Homework_2/Homework_v2.c
+++ 10V/Stefan_Iliev_25/Homework_2/Homework_v2.c
@@ -4,18 +4,18 @@
 int main() 
 {
 //Lucas Numbers
-      unsigned int N1=2;
-      unsigned int N2=1;
-      unsigned int N_sum=0;
-      unsigned int N_number=0;
+      unsigned long N1=2;
+      unsigned long N2=1;
+      unsigned long N_sum=0;
+      unsigned long N_number=0;
          
-	printf("%u\n",N1);
-	printf("%u\n",N2);
+	printf("%lu\n",N1);
+	printf("%lu\n",N2);
 
 	for(N_number;N_number<100;N_number++)
         {
 		N_sum=N1+N2;
-		printf("%u\n",N_sum);
+		printf("%lu\n",N_sum);
 		N1=N2;
 		N2=N_sum;
 	} 
