0eb1ac2835c2af299d59591ad9e804be0d2e8eee
Moretti <moretti@ubuntu.(none)>
2013-10-11 11:20:56 +0300

    homework_3

---
+++ 10G/Moretti_Georgiev_21/homework_3/calc.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int num1,num2,a;
+	char c[4];
+	printf("enter of the operations: add,sub,mul,div,mod\n");
+	scanf("%d %s %d",&num1,c,&num2);
+	if(strcmp(c,"add")==0){
+		a=num1+num2;
+		printf("answer: %d",a);
+	}
+	else if(strcmp(c,"sub")==0){
+		a=num1-num2;
+		printf("answer: %d",a);
+	}
+	else if(strcmp(c,"mul")==0){
+		a=num1*num2;
+		printf("answer: %d",a);
+	}
+	else if(strcmp(c,"div")==0){
+		a=num1/num2;
+		printf("answer: %d",a);
+	}
+	else if(strcmp(c,"mod")==0){
+		a=num1%num2;
+		printf("answer: %d",a);
+	}
+	else{
+		printf("error");
+		
+	}
+
+	return 0;
+}
