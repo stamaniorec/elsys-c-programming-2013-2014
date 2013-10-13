5588318be4040bebe8ededb42e6694129a5d66a1
Hristiyan Dodov <hristiyandodov@ubuntu.(none)>
2013-10-13 19:46:30 +0300

    Homework 3

---
+++ 10V/Hristiyan_Dodov_28/Homework_3/calc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+	int i1;
+	int i2;
+	int i3;
+	char op[3];
+	
+	int error = 0;
+	
+	printf("\n// operatori : add,sub,mul,div,mod //\nIzberi si dve chisla i mejdu tqh sloji operator : ");
+	scanf("%d %s %d", &i1, op, &i2);
+	
+	if (strcmp(op, "add") == 0){
+		i3=i1+i2;
+	}else 
+	if (strcmp(op, "sub") == 0){
+		i3=i1-i2;
+	}else 
+	if (strcmp(op, "mul") == 0){
+		i3=i1*i2;
+	}else 
+	if (strcmp(op, "div") == 0){
+		i3=i1/i2;
+	}else 
+	if (strcmp(op, "mod") == 0){
+		i3=i1%i2;
+	}else{
+		error=1;
+		printf("error\n");}
+	
+	if (error==0){
+		printf("Rezultat : %d \n", i3);
+	}
+	
+	return 0;
+
+}
