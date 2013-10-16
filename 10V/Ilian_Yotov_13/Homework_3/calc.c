a1802cd8b30873a3956317d8b3cd6e6d31b26494
Virtual Ubuntu <yotov@ubuntu.(none)>
2013-10-16 13:29:47 -0700

    Homework3 rework

--- 10V/Ilian_Yotov_13/Homework_3/calc.c
+++ 10V/Ilian_Yotov_13/Homework_3/calc.c
@@ -4,34 +4,34 @@ int main() {
 	char operator[3];
 	int first_num, second_num; 
 	 
-	scanf("%d  %s  %d", &first_num, operator, &second_num);  
+	scanf("%d %s %d", &first_num, operator, &second_num);  
 	
 	if(strcmp (operator, "add") == 0) {
-		printf("%d\n", first_num + second_num);	
+		printf("%d", first_num + second_num);	
 	} 
 	else if(strcmp (operator, "sub") == 0) {
-		printf("%d\n", first_num - second_num);
+		printf("%d", first_num - second_num);
 	} 
 	else if(strcmp (operator, "mul") == 0) {
-		printf("%d\n", first_num * second_num);	
+		printf("%d", first_num * second_num);	
 	} 
 	else if(strcmp (operator, "div") == 0) {
 		if(second_num == 0) {
-			printf("error\n");
+			printf("error");
 		} else {
-	    		printf("%d\n", first_num / second_num);
+	    		printf("%d", first_num / second_num);
 	    	}    		    	
 	} 
 	else if(strcmp (operator, "mod") == 0) {
 		if(second_num == 0) {
-			printf("error\n");
+			printf("error");
 		} else {
-	    		printf("%d\n", first_num % second_num);
+	    		printf("%d", first_num % second_num);
 	    	}	
 	} 
 	else 
 	{
-		printf("error\n");
+		printf("error");
 	}	
 	return 0;
 } 
