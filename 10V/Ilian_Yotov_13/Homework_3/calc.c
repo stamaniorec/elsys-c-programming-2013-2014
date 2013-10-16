a4608d761395eb2cc2f33813f578dcdcf0c1f338
Virtual Ubuntu <yotov@ubuntu.(none)>
2013-10-16 13:15:18 -0700

    Homework3 rework

--- 10V/Ilian_Yotov_13/Homework_3/calc.c
+++ 10V/Ilian_Yotov_13/Homework_3/calc.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
-int main( ) 
-{
+
+int main() {
 	char operator[3];
-	int num1, num2, error = 0;  
-	
-	printf("\n\nIzberi si dve chisla i m/u tqh sloji operator(add, sub, mul, div, mod): ");
-	scanf("%d  %s  %d", &num1, operator, &num2);  
+	int first_num, second_num; 
+	 
+	scanf("%d  %s  %d", &first_num, operator, &second_num);  
 	
 	if(strcmp (operator, "add") == 0) {
-		printf("Rezultat : %d \n\n", num1+num2);
-		
-	} else if(strcmp (operator, "sub") == 0) {
-		printf("Rezultat : %d \n\n", num1-num2);
-		
-	} else if(strcmp (operator, "mul") == 0) {
-		printf("Rezultat : %d \n\n", num1*num2);
-		
-	} else if(strcmp (operator, "div") == 0) {
-	    	printf("Rezultat : %d \n\n", num1/num2);
-	    	
-	} else if(strcmp (operator, "mod") == 0) {
-	    	printf("Rezultat : %d \n\n", num1%num2);
-	    	
-	} else {
-		printf("Greshka pri presmqtaneto!\n\n");
+		printf("%d\n", first_num + second_num);	
+	} 
+	else if(strcmp (operator, "sub") == 0) {
+		printf("%d\n", first_num - second_num);
+	} 
+	else if(strcmp (operator, "mul") == 0) {
+		printf("%d\n", first_num * second_num);	
+	} 
+	else if(strcmp (operator, "div") == 0) {
+		if(second_num == 0) {
+			printf("error\n");
+		} else {
+	    		printf("%d\n", first_num / second_num);
+	    	}    		    	
+	} 
+	else if(strcmp (operator, "mod") == 0) {
+		if(second_num == 0) {
+			printf("error\n");
+		} else {
+	    		printf("%d\n", first_num % second_num);
+	    	}	
+	} 
+	else 
+	{
+		printf("error\n");
 	}	
 	return 0;
 } 
