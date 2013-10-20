6ada7a683d98e49a0c0073f8622cd9593fc7360e
kingsize1 <kingsize@ubuntu.(none)>
2013-10-20 12:36:17 +0300

    Homework_1

--- 10G/Borislav_Rusinov/Homework_3/calc.c
+++ 10G/Borislav_Rusinov/Homework_3/calc.c
@@ -2,53 +2,67 @@
 #include <string.h>
 int main () 
 {
-	int asd;
+	int checker;
 	int a,b,c;
-	char  operacia[4];
+	char  operation[4];
 	char  add[4]="add";
 	char 	sub[4]="sub";
 	char mul[4]="mul";
 	char div[4]="div";
 	char mod[4]="mod";
-	printf("vuvedi purvo chislo:");
+	printf("first number:");
 	scanf("%d",&a);
-	printf("vuvedi znak za operaciqta:");
-	scanf("%s",&operacia);
-	printf("vuvedi vtoro chislo:");
+	printf("operation:");
+	scanf("%s",&operation);
+	printf("second number:");
 	scanf("%d",&b);
 	c=0;
-	asd=0;
-	 if (strcmp(operacia,add)==0)
+	checker=0;
+	 if (strcmp(operation,add)==0)
 	{
 		c=a+b;
 		printf("%d\n",c);
-		asd=1;	
+		checker=1;	
 	}
-	if (strcmp(operacia,sub)==0)
+	if (strcmp(operation,sub)==0)
 	{
 		c=b-a;
 		printf("%d\n",c);
-		asd=1;	
+		checker=1;	
 	}
-	if (strcmp(operacia,mul)==0)
+	if (strcmp(operation,mul)==0)
 	{
 		c=a*b;
 		printf("%d\n",c);
-		asd=1;	
+		checker=1;	
 	}
-	if (strcmp(operacia,div)==0)
+	if (strcmp(operation,div)==0)
 	{
+		if (b==0) 
+		{
+			printf("error\n");
+			checker=1;
+		}else
+		{
 		c=a/b;
 		printf("%d\n",c);
-		asd=1;	
+		checker=1;
+		}	
 	}
-	if (strcmp(operacia,mod)==0)
+	if (strcmp(operation,mod)==0)
 	{
+		if (b==0)
+		{
+			printf("error\n");
+			checker=1;		
+		}else 
+		{
 		c=a%b;
 		printf("%d\n",c);
-		asd=1;
+		checker=1;
+		}
 	}	
-	if (asd==0)
+	if (checker==0)
 	{
 		printf("error\n");	
 	}	 
---
+++ 10G/Borislav_Rusinov_2/Homework_1/domashno1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+	char c=0;
+	int i=0;
+
+		for (i=0; i<=255; i++) {
+			printf ("%c - %d\n",c,i );
+			c++;
+		}
+		
+
+		return 0;
+		}
