43ccfea38d0bf4cc6a95b8dc98f1028ef1061815
Marina97 <marianbelchev@gmail.com>
2013-10-21 20:41:07 +0300

    Homework3 - calculator

---
+++ 10G/Marina_Markova_19/homework_3/calc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int main()
+{
+        char opr[3];
+        int a;
+        int b;
+        scanf("%d %s %d",&a,opr,&b);
+        if (strcmp(opr,"sub")==0){
+                printf("%d", a-b);
+        } else
+                if (strcmp(opr,"add")==0){ printf("%d", a+b);
+        } else
+                if (strcmp(opr,"mul")==0){ printf("%d", a*b);
+        } else
+                if (strcmp(opr,"div")==0){
+                        if (b!=0){
+                                printf("%d", a/b);
+                        } else
+                                printf("error");
+        } else
+                if (strcmp(opr,"mod")==0){
+                        if (b!=0){
+                                printf("%d", a%b);
+                        } else
+                                printf("error");
+        } else {
+                printf ("error");
+        }
+        return 0;
+}
