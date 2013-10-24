6a1034815b48190038a415f57f7493e3153a041c
stefcho <junkmen@ubuntu.(none)>
2013-10-24 23:42:07 +0300

    homework_4 has been completed.

---
+++ 10V/Stefan_Iliev_25/homework_4/matrix.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int n_numbers, i, j, numbers_sum = 0, input_numbers[100];
+	 
+	
+ 	do
+ 	{	
+ 	 scanf("%d",&n_numbers);
+
+ 	}while(n_numbers < 0 ||	n_numbers >100);
+
+	for (i=1; i<n_numbers; i++)
+	{
+		for(j=0; j<n_numbers; j++)
+		{
+			scanf("%d ",&input_numbers[j]);
+			if (j >= i) {
+			numbers_sum += input_numbers[j];
+			}
+		}
+		
+	}
+
+
+	
+	printf("%d\n",numbers_sum);
+
+
+	return 0;
+}
