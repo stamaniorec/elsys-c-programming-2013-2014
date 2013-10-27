bf4cc999dd48928a687cfe15b4a4a0657dba4007
KaloianDoichinov <kaloian_doichinov@abv.bg>
2013-10-27 20:34:02 +0200

    Update matrix.c

--- 10V/Kaloian_Doichinov_17/Homework_4/matrix.c
+++ 10V/Kaloian_Doichinov_17/Homework_4/matrix.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-        int n,row,column;
+        int a,row,column;
         float matrix[100][100],sum=0;
 
-        scanf("%d",&n);
-        if (n<=100) {
+        scanf("%d",&a);
+        if (a<=100) {
                 for (row=0; row<=n-1; row++) {
-                        for (column=0; column<=n-1; column++) {
+                        for (column=0; column<=a-1; column++) {
                                 scanf("%f",&matrix[row][column]);
                                 if (column>row) sum +=matrix[row][column];
                         }
                 }        
                 printf("%f",sum);
         } else {
-                printf("error");
+                printf("Error");
         }
         
         return 0;
