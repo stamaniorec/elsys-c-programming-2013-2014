0d6098ef5cd8ee6c9513a4ad46c5c98cd62d39dc
ivailo <la4ko@ubuntu.(none)>
2013-10-29 01:00:43 +0200

    Homework 4 fix_Lachezar

--- 10G/Lachezar_Sandev_17/homework_4/matrix.c
+++ 10G/Lachezar_Sandev_17/homework_4/matrix.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
 int main(){
-	void sum=0, j, column, m;
+	int sum=0, j, column, m;
 	scanf("%d",&m);
- 	void arr[m][m]
+ 	int arr[m][m];
 
-  if (m<=100){
-   for (j=1; j<=m; j++)
+  if (m<=100 && m>0){
+   for (j=0; j<m; j++)
    {
-    for (column=0; column<=m; column++)
+    for (column=0; column<m; column++)
     {scanf ("%d", &arr[j][column]);}
   }
-   for (j=0; j<=m; j++)
-   {
-    for (column=2; column<=m; column++)
-    {
-     if (j<column) {sum += sum+arr[j][column]; } 
+  
+  for (j=0; j<m; j++){
+    for (column=0; column<m; column++){
+     if (j<column) 
+
+	sum += sum+arr[j][column];}
     	}   
-    }
-
-  printf("%d\n", sum);
-
-  }
-  else 
-	printf ("error\m");
-
+    
+  printf("%d\n", sum);}
+  else {
+	printf ("error\n");
+}
 
 return 0;
 }
