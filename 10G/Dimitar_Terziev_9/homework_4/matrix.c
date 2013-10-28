93e9de3fb6c7bec34eb718701c95bf835bf7ee1e
Mitko Viki <mitkoviki@ubuntu.(none)>
2013-10-28 08:50:23 +0200

    matrix

--- 10G/Dimitar_Terziev_9/homework_4/matrix.c
+++ 10G/Dimitar_Terziev_9/homework_4/matrix.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int main () {
-	int matrix[100][100];
-	int n, counter_1, counter_2,sum=0;
+	float matrix[100][100];
+	int n, counter_1, counter_2;
+	float sum=0;
 	scanf("%d", &n);
+	if(n>100){
+		while(n>100){
+			scanf("%d", &n);
+		}
+	}
 	for(counter_1=0;counter_1<n;++counter_1){
 			for(counter_2=0;counter_2<n;++counter_2){
-				scanf("%d",&matrix[counter_1][counter_2]);
+				scanf("%f",&matrix[counter_1][counter_2]);
 			}
 		}
 	counter_1=0;
@@ -13,15 +19,15 @@ int main () {
 		counter_2=counter_1;
 		while(counter_2<n){
 			counter_2++;
-			sum+=matrix[counter_1][counter_2];
+			sum=sum+matrix[counter_1][counter_2];
 		}
 		counter_1++;
 	}
-	printf("%d",sum);
+	printf("%f\n",sum);
 	return 0;
 }
 
-
+	//float calculations are not always accurate
 
 	/*counter_1=0;
 	while(counter_1<n){
